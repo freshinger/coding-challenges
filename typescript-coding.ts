7cd9e73b811c4859f4f441f301d4e7327f305aba
Daniel <daniel_ciecior@yahoo.de>
2025-06-26 12:21:09 +0200

    coding 3

--- typescript-coding.ts
+++ typescript-coding.ts
@@ -22,3 +22,11 @@ let filtered = filterlongwords(words);
 for (const word of filtered) {
   console.log(word);
 }
+
+// step 3
+
+let numbersToReduce: number[] = [1, 2, 3, 4];
+function sumNumbers(input: number[]): number {
+  return input.reduce((x, y) => x + y);
+}
+console.log(sumNumbers(numbersToReduce));
