fd2f85294296ffcce433a410ecd199ac33baef09
Daniel <daniel_ciecior@yahoo.de>
2025-06-26 15:33:06 +0200

    save

--- typescript-arrayfunctions.js
+++ typescript-arrayfunctions.js
@@ -1,9 +1,16 @@
 "use strict";
-const list = [15, 6, 3213, 9, 0, 12, 8464, 1, 1264, 481, 186, 1031, 194];
-// Sort these numbers in descending order.
-console.log(list.sort((a, b) => b - a));
-//Square these numbers.
-console.log(list.map((x) => x * x));
-//Remove the lowest two numbers and the highest 4 numbers from the list.
-console.log(list);
-console.log(list.filter((x) => list.indexOf(x) != 0));
+let list1 = [15, 6, 3213, 9, 0, 12, 8464, 1, 1264, 481, 186, 1031, 194];
+list1.sort((a, b) => b - a);
+console.log(list1);
+list1 = list1.map((x) => x * x);
+console.log(list1);
+let filtered1 = list1.filter((x) => list1.indexOf(x) != 0 &&
+    list1.indexOf(x) != 1 &&
+    list1.indexOf(x) != 2 &&
+    list1.indexOf(x) != 3 &&
+    list1.indexOf(x) != list1.length - 1 &&
+    list1.indexOf(x) != list1.length - 2);
+console.log(filtered1);
+let filtered2 = filtered1.filter((x) => x % 4 !== 0);
+console.log(filtered2);
+console.log(filtered2.reduce((x, y) => x + y));
