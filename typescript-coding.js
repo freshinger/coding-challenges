8f5c454c35204cbf7ef949b2e1e3e9443bd033b7
Daniel <daniel_ciecior@yahoo.de>
2025-06-26 12:14:14 +0200

    coding 2

--- typescript-coding.js
+++ typescript-coding.js
@@ -2,9 +2,18 @@
 // step 1
 let numbers = [1, 2, 3, 4];
 function dbl(input) {
-    return input.map((x) => x * 2);
+  return input.map((x) => x * 2);
 }
 let dblnumbers = dbl(numbers);
 for (const element of dblnumbers) {
-    console.log(element);
+  console.log(element);
+}
+//step 2
+let words = ["it", "is", "the", "longest", "shortest", "word"];
+function filterlongwords(input) {
+  return input.filter((x) => x.length > 5);
+}
+let filtered = filterlongwords(words);
+for (const word of filtered) {
+  console.log(word);
 }
--- typescript-coding.ts
+++ typescript-coding.ts
@@ -8,3 +8,17 @@ let dblnumbers = dbl(numbers);
 for (const element of dblnumbers) {
   console.log(element);
 }
+
+//step 2
+
+let words: string[] = ["it", "is", "the", "longest", "shortest", "word"];
+
+function filterlongwords(input: string[]): string[] {
+  return input.filter((x) => x.length > 5);
+}
+
+let filtered = filterlongwords(words);
+
+for (const word of filtered) {
+  console.log(word);
+}
